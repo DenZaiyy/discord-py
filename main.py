56c6001575abf604c063ffa9e2d5d2740ddf31e7
DenZaiyy <denzaiyy@outlook.fr>
2024-04-10 03:10:19 +0200

    chore: refactoring loading commands/events

--- main.py
+++ main.py
@@ -3,39 +3,48 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
-from discord.ext.commands import has_permissions
 import logging
 
 # Load the environment variables
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-PREFIX: Final[str] = os.getenv('DISCORD_PREFIX')
+PREFIX: Final[str] = os.getenv('DEFAULT_PREFIX')
 
 # Setup the client
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
-bot.activity = discord.Game(name='!help to more infos')
+bot.activity = discord.Game(name=f'{PREFIX}help')
 
 # Setup logs
 handler = logging.FileHandler(filename='logs.log', encoding='utf-8', mode='w')
 
-# Event to load all commands available in commands folder
+async def load_commands():
+    try:
+        for filename in os.listdir('./commands'):
+            if filename.endswith('.py'):
+                await bot.load_extension(f'commands.{filename[:-3]}')
+                print(f'Loaded command: {filename[:-3]}')
+    except Exception as e:
+        print(f'An error occurred while loading commands: {e}')
+
+async def load_events():
+    try:
+        for filename in os.listdir('./events'):
+            if filename.endswith('.py'):
+                await bot.load_extension(f'events.{filename[:-3]}')
+                print(f'Loaded event: {filename[:-3]}')
+    except Exception as e:
+        print(f'An error occurred while loading events: {e}')
+
 @bot.event
 async def on_ready() -> None:
-    # Load all commands in the commands folder
-    for filename in os.listdir('./commands'):
-        if filename.endswith('.py'):
-            await bot.load_extension(f'commands.{filename[:-3]}')
-            print(f'Loaded command: {filename[:-3]}')
+    try:
+        await load_commands()
+        await load_events()
 
-    # Load all events in the events folder
-    for filename in os.listdir('./events'):
-        if filename.endswith('.py'):
-            await bot.load_extension(f'events.{filename[:-3]}')
-            print(f'Loaded event: {filename[:-3]}')
-
-    # Print bot name when connected
-    print(f'🤖 {bot.user.name} has connected (with prefix: {PREFIX})! 🤖')
+        print(f'🤖 {bot.user.name} has connected! 🤖')
+    except Exception as e:
+        print(f'An error occurred with on_ready event: {e}')
 
 # main entry point
 def main() -> None:
